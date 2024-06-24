ed43e06aad045042b4a2d63356438e85bda5c177
richiips <richolivares.m@gmail.com>
2024-06-24 16:20:45 -0400

    ejercicio cambio color div listo

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function pintar(elemento, color = 'green'){
+    elemento.style.backgroundColor = color;
+}
+
+const ele = document.getElementById("ele1")
+ele.addEventListener("click", function() {
+    pintar(ele, 'yellow');
+});
